8ee435e14e9a91cd0696fc2aad8303f0a0e132c7
Zex Li <top_zlynch@yahoo.com>
2017-04-09 23:59:48 +0800

    add linear regression

--- apps/train_test.py
+++ apps/train_test.py
@@ -5,7 +5,28 @@ from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 from apps.utils import plot_decision_regions
+
+def logistic_regression(X_train_std, y_train, X_test_std, y_test):
+    lr = LogisticRegression(C=1000.0, random_state=0)
+    lr.fit(X_train_std, y_train)
+    X_comb_std = np.vstack((X_train_std, X_test_std))
+    y_comb = np.hstack((y_train, y_test))
+    plot_decision_regions(X_comb_std, y_comb, classifier=lr, test_idx=range(105, 150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+def decision_regions(X_train_std, y_train, X_test_std, y_test, classifier):
+    X_comb_std = np.vstack((X_train_std, X_test_std))
+    y_comb = np.hstack((y_train, y_test))
+    plot_decision_regions(X_comb_std, y_comb, classifier=classifier, test_idx=range(105, 150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
 
 def selftest():
     iris = datasets.load_iris()
@@ -21,13 +42,8 @@ def selftest():
     y_pred = ppn.predict(X_test_std)
     print('Misclassified samples: %d' % (y_test!=y_pred).sum())
     print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
-    X_comb_std = np.vstack((X_train_std, X_test_std))
-    y_comb = np.hstack((y_train, y_test))
-    plot_decision_regions(X_comb_std, y_comb, classifier=ppn, test_idx=range(105, 150))
-    plt.xlabel('petal length [standardized]')
-    plt.ylabel('petal width [standardized]')
-    plt.legend(loc='upper left')
-    plt.show()
+    decision_regions(X_train_std, y_train, X_test_std, y_test, ppn)
+    logistic_regression(X_train_std, y_train, X_test_std, y_test)
 
 if __name__ == '__main__':
     selftest()
