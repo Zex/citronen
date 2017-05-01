13dd5e379b0093bd18dad30883aad6488da442d0
Zex Li <zex.li@blackboard.com>
2017-05-01 23:10:05 +0800

    plot from csv

--- apps/show_loss.py
+++ apps/show_loss.py
@@ -1,26 +1,10 @@
+import matplotlib
+matplotlib.use("TkAgg")
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 import re
 import sys
-
-def show_losses(losses):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(np.arange(len(losses)), losses)
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
-    plt.title('Loss/Iteration')
-    return fig, ax
-
-def show_accuracy(accuracy):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(np.arange(len(accuracy)), accuracy)
-    plt.xlabel('epoch')
-    plt.ylabel('accuracy')
-    plt.title('Accuracy')
-    return fig, ax
 
 def get_accu(line):
     # score: 0.08709494
@@ -28,7 +12,6 @@ def get_accu(line):
     if accu is None:
         return None
     accu = accu.group().split(':')[1]
-    print('accuracy:{}'.format(accu))
     return float(accu)
 
 def get_loss(line):
@@ -38,38 +21,36 @@ def get_loss(line):
     if iteration is not None and loss is not None:
         iteration, loss = iteration.group(), loss.group()
         loss = loss.split(' ')[2]
-        if len(loss) == 0:
-            return None
-        print('iteration:{}, loss:{}'.format(iteration, loss))
-    else:
-        return None
     return float(loss)
 
-def pipe_stdin_loss():
-    losses = []
+def pipe_csv(ax, fig):
+    losses, accs, cates, sparses = [], [], [], []
     while True:
         line = sys.stdin.readline()
         if line is None or len(line) == 0:
             break
-        loss = get_loss(line)
-        if loss is not None:
-            losses.append(loss)
-    show_losses(losses)
-    plt.show()
+        loss, acc, cate, sparse = line.split(',')
+        losses.append(float(loss))
+        accs.append(float(acc))
+        cates.append(float(cate))
+        sparses.append(float(sparse))
+        pylab.plot(np.arange(len(losses)), losses, '.', color='blue', markerfacecolor='blue')
+        pylab.plot(np.arange(len(accs)), accs, '.', color='red', markerfacecolor='red')
+        pylab.plot(np.arange(len(cates)), cates, '.', color='green', markerfacecolor='green')
+        pylab.plot(np.arange(len(sparses)), sparses, '.', color='yellow', markerfacecolor='yellow')
+        ax.legend(['loss', 'acc', 'cate_acc', 'sparse_cate_acc'], loc='upper left')
+        fig.canvas.draw()
 
-def pipe_stdin_accu():
-    accuracy = []
-    while True:
-        line = sys.stdin.readline()
-        print(line)
-        if line is None or len(line) == 0:
-            break
-        accu = get_accu(line)
-        if accu is not None:
-            accuracy.append(accu)
-    show_accuracy(accuracy)
-    plt.show()
-
-pipe_stdin_loss()
-#pipe_stdin_accu()
+if __name__ == '__main__':
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, facecolor='black')
+    ax.autoscale(True)
+    plt.title('profile')
+    plt.ylabel('metrics')
+    plt.xlabel('steps')
+    fig.show()
+    #pipe_stdin_loss()
+    #pipe_stdin_accu()
+    pipe_csv(ax, fig)
 
