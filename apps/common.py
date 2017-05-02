493a665929a38517cb0cf45fb929dadd3d77f227
Zex Li <zex.li@blackboard.com>
2017-05-02 19:51:04 +0800

    add common

---
+++ apps/common.py
@@ -0,0 +1,11 @@
+from keras.callbacks import Callback
+
+class PlotLog(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accs = []
+
+    def on_batch_end(self, batch, logs={}):
+        PlotLog.logfd.write('{},{},{},{}\n'.format(logs.get('loss'), logs.get('acc'), logs.get('binary_accuracy'), logs.get('sparse_categorical_accuracy')))
+        PlotLog.logfd.flush()
+
