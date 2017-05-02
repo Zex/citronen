9fd54e3664b25dff0cd6b06e241a5a1fd776ca2c
Zex Li <zex.li@blackboard.com>
2017-05-02 13:47:11 +0800

    add total var

--- apps/plot_from_tfevent.py
+++ apps/plot_from_tfevent.py
@@ -16,15 +16,19 @@ def plot_from_tfevent(tfevent_base):
     evacc = event_accumulator.EventAccumulator(tfevent_base)
     evacc.Reload()
     print("path:{} file version:{}".format(tfevent_base, evacc.file_version))
-    total = len(evacc._scalars.Keys())
+    plot_scalars(evacc._scalars)
 
-    for k in evacc._scalars.Keys():
-        metric = evacc._scalars.Items(k)
+def plot_scalars(scalars):
+    total = len(scalars.Keys())
+    for k in scalars.Keys():
+        metric = scalars.Items(k)
+        np.random.shuffle(marks)
+        np.random.shuffle(colors)
         pylab.plot(np.arange(len(metric)), [l.value for l in metric], 
                 marks[np.random.randint(total)],
-                color=colors[np.random.randint(total)],
-                mfc=mfcs[np.random.randint(total)])
-    pylab.legend(evacc._scalars.Keys(), loc='upper right')
+                color=colors[np.random.randint(len(colors))],
+                mfc=mfcs[np.random.randint(len(mfcs))])
+    pylab.legend(scalars.Keys(), loc='upper right')
     pylab.xlabel('epoch')
     pylab.ylabel('metrics')
     pylab.title('TF Event Metrics')
