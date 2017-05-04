5e156895fecdfec6b1a2818683cdbf38cee30f44
Zex Li <top_zlynch@yahoo.com>
2017-05-04 11:50:58 +0800

    StateReport

--- apps/common.py
+++ apps/common.py
@@ -1,11 +1,26 @@
 from keras.callbacks import Callback
 
-class PlotLog(Callback):
-    def on_train_begin(self, logs={}):
-        self.losses = []
-        self.accs = []
+class StateReport(Callback):
+    def __init__(self, output_path):
+        self.w = open(output_path, 'w+')
 
-    def on_batch_end(self, batch, logs={}):
-        PlotLog.logfd.write('{},{},{},{}\n'.format(logs.get('loss'), logs.get('acc'), logs.get('binary_accuracy'), logs.get('sparse_categorical_accuracy')))
-        PlotLog.logfd.flush()
+    def on_epoch_begin(self, epoch, logs=None):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.w.write('epoch:{} '.format(epoch)+''.join(['{}:{} '.format(k, v) for k,v in logs.items()])+'\n')
+        #self.w.write('{},{},{},{}\n'.format(logs.get('loss'), logs.get('acc'), logs.get('binary_accuracy'), logs.get('sparse_categorical_accuracy')))
+
+    def on_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_batch_end(self, batch, logs=None):
+        self.w.write('batch:{} '.format(batch)+''.join(['{}:{} '.format(k, v) for k,v in logs.items()])+'\n')
+
+    def on_train_begin(self, logs=None):
+        pass
+
+    def on_train_end(self, logs=None):
+        self.w.write(''.join(['{}:{} '.format(k, v) for k,v in logs.items()])+'\n')
+
 
