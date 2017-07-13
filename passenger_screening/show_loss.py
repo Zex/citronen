7df25ae4868c318b21e29981f038121287e5c402
Zex Li <top_zlynch@yahoo.com>
2017-07-13 17:56:10 +0800

    add window size arg

--- passenger_screening/show_loss.py
+++ passenger_screening/show_loss.py
@@ -16,7 +16,7 @@ def get_accu(line):
   accu = accu.group().split(':')[1]
   return float(accu)
 
-def get_loss(line):
+def get_loss(line, emp=10):
   #Iteration 83, loss = 0.08709494
   line = line.strip('\n')
   epoch, loss = re.search('\[[\d]+\]', line), re.search('loss: [0-9]*\.[0-9]+', line)
@@ -24,24 +24,24 @@ def get_loss(line):
     epoch = epoch.group()[1]
   if loss:
     loss = loss.group().split(' ')[1]
-  return epoch, np.round(float(loss)*100, 4)
+  return epoch, np.round(float(loss)*emp, 4)
 
-def pipe_loss(flow=False, trim_level=None, stage_size=1000):
+def pipe_loss(flow=False, trim_level=None, stage_size=1000, emp=10, window_size=100):
   global fig, ax1, ax2
   losses, accs = [], []
   while True:
     line = sys.stdin.readline()
     if line is None or len(line) == 0:
         break
-    epoch, loss = get_loss(line)
+    epoch, loss = get_loss(line, emp)
     print('epoch:{}, loss:{}'.format(epoch, loss))
     if trim_level and loss > trim_level:
       continue
     losses.append(loss)
 
     if flow:
-      if len(losses) > 100:
-        sub_losses = losses[-100:]
+      if len(losses) > window_size:
+        sub_losses = losses[-window_size:]
       else:
         sub_losses = losses[:]
       marker = '-'
@@ -56,9 +56,8 @@ def pipe_loss(flow=False, trim_level=None, stage_size=1000):
       marker = '.'
       ax2.cla()
       ax2.plot(np.arange(len(losses)), losses, marker, color='blue', markerfacecolor='blue')
-
-    plt.title('[{}] {}/{}'.format(epoch, len(losses), loss))
-    fig.canvas.draw()
+      plt.title('[{}] {}/{}'.format(epoch, len(losses), loss))
+      fig.canvas.draw()
 
 
 if __name__ == '__main__':
@@ -74,10 +73,16 @@ if __name__ == '__main__':
   plt.xlabel('steps')
   fig.show()
   parser = argparse.ArgumentParser()
-  parser.add_argument('--flow', default=0, type=int)
+  parser.add_argument('--flow', default=False, type=bool)
   parser.add_argument('--trim_level', default=None, type=int)
   parser.add_argument('--stage_size', default=1000, type=int)
+  parser.add_argument('--window_size', default=100, type=int)
+  parser.add_argument('--emp', default=10, type=int)
   args = parser.parse_args()
-  pipe_loss(args.flow, args.trim_level, args.stage_size)
-  time.sleep(10000)
+
+  try:
+    pipe_loss(args.flow, args.trim_level, args.stage_size, args.emp, args.window_size)
+    time.sleep(10000)
+  except KeyboardInterrupt:
+    fig.close()
 
