025e9fcf83ac5048be96bdac95acc8c26a9ffeb1
Zex Li <top_zlynch@yahoo.com>
2017-08-03 17:55:50 +0800

    update plot

--- passenger_screening/post_loss.py
+++ passenger_screening/post_loss.py
@@ -69,9 +69,9 @@ def pipe_loss(flow=False, trim_level=None, stage_size=1000, emp=10, window_size=
     if len(losses) % stage_size != 0:
       continue
     else:
-      marker = 'o'
+      marker, color = '--', 'blue'
       ax2.cla()
-      ax2.plot(np.arange(len(losses)), losses, marker, color='blue', markerfacecolor='blue')
+      ax2.plot(np.arange(len(losses)), losses, marker, color=color, markerfacecolor=color)
       if with_acc:
           marker, color = '--', 'red'
           ax2.plot(np.arange(len(accs)), accs, marker, color=color, markerfacecolor=color)
