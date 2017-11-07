93caaf0ec151e4009e750a4708002b99481f7d2a
Zex Li <top_zlynch@yahoo.com>
2017-11-07 14:31:29 +0800

    cleanup

--- springer/db_utils.py
+++ springer/db_utils.py
@@ -5,28 +5,24 @@ import logging
 import numpy as np
 import time
 
-lgr = logging.getLogger()
-
 def unify(text):
     text = text.strip()
 
+    if not text:
+        return text
     if re.match(r"(\d+)-(\d*)$", text): # perfect
         p1, p2 = text.split('-')
         if p1 and p2 and int(p2) < int(p1):
             text = "{}-{}{}".format(p1, p1[:len(p2)], p2)
-        lgr.info("[P] {}".format(text))
         return text
     if re.match(r"(\d+)$", text): # fine
-        lgr.info("[F] {}".format(text))
         return text
     if re.match(r"(\d+)-(\d*).(\d*)e+(\d*)", text): # ends with float
-        lgr.info("[EwF] {}".format(text))
         return "{}{}".format(text[:text.index('-')+1], str(int(float(text.split('-')[1]))))
     if re.match(r"(\d+)/(\d+)-(\d+)", text): # discontinuous range
-        lgr.info("[DCR] {}".format(text))
         return "{}-{}".format(text.split('/')[0], text.split('-')[1])
     lgr.warn("[UNHANDLED] {}".format(text))
-    return None # unhandled
+    return text # unhandled
 
 cases = [
     "334-",
@@ -36,4 +32,5 @@ cases = [
     "32/37-99",
     "333"
     ]
+
 print(np.vectorize(unify)(cases))
