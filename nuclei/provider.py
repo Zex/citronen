007cf7d9ce8fdec451454a417415bf071b4ccb3c
Zex Li <top_zlynch@yahoo.com>
2018-02-02 15:15:35 +0800

    encode mask

--- nuclei/provider.py
+++ nuclei/provider.py
@@ -112,21 +112,42 @@ def plot_target(target, shape, ax=None):
 
     start = [int(t) for i, t in enumerate(seq) if i % 2 == 0] 
     count = [int(t) for i, t in enumerate(seq) if i % 2 != 0]
-    mask = np.zeros(shape[0]*shape[1])
+    mask = np.zeros(shape[1]*shape[0])
 
     for s, c in zip(start, count):
-        ind = s
-        while ind < s+c:
+        for ind in range(s, s+c+1):
+            if ind >= mask.shape[0]:
+                continue
             mask[ind] = 255
-            ind += 1
 
-    mask = mask.reshape(shape[0], shape[1])
+    mask = mask.reshape(shape[1], shape[0])
+    encode = encode_mask(mask) 
+    #print(target == encode)
 
     if ax:
         ax.imshow(mask)
-        ax.scatter(x, y, linewidths=2, s=1)
-        
+        #ax.scatter(start, count, linewidths=2, s=1)
+     
     return mask
+
+
+def encode_mask(mask):
+    mask = mask.flatten()
+    nonzero = mask.nonzero()[0]
+    bucket, target = [], []
+
+    for cell in nonzero:
+        if not bucket:
+            bucket.append(cell)
+            continue
+        if cell-1 not in bucket:
+            target.extend([str(bucket[0]), str(len(bucket)-1)])
+            bucket.clear()
+        bucket.append(cell)
+
+    target.extend([str(bucket[0]), str(len(bucket)-1)])
+
+    return ' '.join(target)
 
 
 def start():
@@ -138,5 +159,7 @@ def start():
 if __name__ == '__main__':
     #start()
     target = "66 35 322 36 578 38 834 39 1090 41 1346 42 1602 44 1858 45 2113 48 2367 52 2622 54 2878 55 3134 56 3390 57 3646 57 3902 58 4158 59 4414 59 4670 59 4926 60 5182 60 5438 61 5695 60 5951 60 6207 60 6463 60 6719 61 6975 61 7232 61 7488 61 7744 61 8000 61 8256 61 8513 60 8769 60 9026 59 9282 59 9539 58 9795 58 10051 58 10307 58 10564 57 10820 57 11076 57 11332 56 11588 56 11845 54 12102 52 12359 51 12616 50 12872 50 13129 48 13386 47 13643 45 13900 42 14157 40 14414 39 14671 37 14929 34 15186 30 15443 24 15700 22 15962 6 15972 5"
-    plot_target(target, (256, 256, 4), None)
+    target = "74746 7 74998 11 75253 12 75507 14 75762 15 76016 17 76271 18 76526 19 76781 20 77036 21 77291 22 77546 23 77802 23 78058 23 78313 24 78569 24 78825 24 79081 24 79336 25 79592 25 79848 25 80104 25 80361 24 80617 24 80874 23 81131 22 81388 21 81644 21 81901 20"
+    plot_target(target, (256, 320, 4), None)
+    plt.show()
 
