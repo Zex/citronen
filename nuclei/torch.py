145bd9413576ec6238fe831d5eaac4289e652396
Zex Li <top_zlynch@yahoo.com>
2018-02-05 18:45:17 +0800

    cleanup

--- nuclei/torch.py
+++ nuclei/torch.py
@@ -61,42 +61,21 @@ class Runner(object):
         self.prov = Provider()
         self.height, self.width = self.prov.height, self.prov.width
         self.channel = self.prov.channel
-        self.device = "/cpu:0"
+#        self.device = "/cpu:0"
 
     def _build_model(self):
         self.model = TC()
 
     def train(self):
         self._build_model()
-        #self.estimator.train(input_fn=preprocess, steps=100)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self.summary_writer = tf.summary.FileWriter(self.log_path, sess.graph)
-            self.saver = tf.train.Saver(tf.global_variables())
 
-            for e in range(self.epochs):
-                self.foreach_epoch(sess, e)
+        for e in range(self.epochs):
+            self.foreach_epoch(sess, e)
 
     def foreach_epoch(self, sess, e):
         for X, y, total_nuclei in self.prov.gen_data():
-            feed_dict = {
-                self.input_x: X,
-                self.input_y: y,
-                self.dropout_keep: 1-self.dropout_rate,
-                self.total_nuclei: total_nuclei,
-                }
-
-            _, loss, step, summ = sess.run(\
-                [self.train_op, self.loss, self.global_step, self.summary],\
-                feed_dict=feed_dict)
-
-            if step % self.summ_intv == 0:
-                print('++ [step-{}] loss:{}'.format(step, loss), flush=True)
-                self.summary_writer.add_summary(summ, step)
-                self.saver.save(sess, self.model_dir+'/cnn',
-                            global_step=tf.train.global_step(sess, self.global_step))
-                self.inner_test(sess, step)
-        
+            output = self.model(X)
+            
                 
     def inner_test(self, sess, step):
         pass
