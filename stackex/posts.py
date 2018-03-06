362e37f16e4976ba380df6a4dd2aadb2643f29b9
Zex Li <top_zlynch@yahoo.com>
2018-03-06 16:52:01 +0800

    define P and Q

--- stackex/posts.py
+++ stackex/posts.py
@@ -6,6 +6,7 @@ import re
 from lxml import etree
 import pickle
 import numpy as np
+import ujson
 
 from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
@@ -22,17 +23,72 @@ def from_pickle(path):
     return ret
 
 
+class P:
+
+    def __init__(self, z_dim, h_dim, x_dim):
+        self.z_dim = z_dim
+        self.h_dim = h_dim
+        self.x_dim = x_dim
+
+        with tf.name_scope('P'):
+            self.w1 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.z_dim, self.h_dim]))
+            self.b1 = tf.Variable(tf.zeros([self.h_dim,]))
+            self.w2 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.x_dim]))
+            self.b2 = tf.Variable(tf.zeros([self.h_dim,]))
+
+    def __call__(self, z):
+        with tf.name_scope('P'):
+            h = tf.nn.relu(tf.nn.xw_plus_b(z, self.w1, self.b1, name='l_1'))
+            logits = tf.nn.xw_plus_b(h, self.w2, self.b2, name='l_2')
+            prob = tf.nn.sigmoid(logits)
+        return prob, logits
+
+    def var_list(self):
+        return [self.w1, self.b1, self.w2, self.b2]
+
+
+class Q:
+
+    def __init__(self, z_dim, h_dim, x_dim):
+        self.z_dim = z_dim
+        self.h_dim = h_dim
+        self.x_dim = x_dim
+
+        with tf.name_scope('Q'):
+            self.w1 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.x_dim, self.h_dim]))
+            self.b1 = tf.Variable(tf.zeros([self.h_dim,]))
+            self.w2 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.z_dim]))
+            self.b2 = tf.Variable(tf.zeros([self.z_dim,]))
+
+    def __call__(self, X):
+        with tf.name_scope('Q'):
+            h = tf.nn.relu(tf.nn.xw_plus_b(X, self.w1, self.b1, name='l_1'))
+            z = tf.nn.relu(tf.nn.xw_plus_b(h, self.w2, self.b2, name='l_1'))
+        return z
+
+    def var_list(self):
+        return [self.w1, self.b1, self.w2, self.b2]
+
+
 class StackEx(object):
 
     def __init__(self):
+        self.max_doc_len = 128
+
+        self.z_dim = 10
+        self.x_dim = self.max_doc_len
+        self.h_dim = 128
+
+        self.init_step = 0
         self.data_path = "data/ai.stackexchange.com/Posts.xml"
         self.batch_size = 32
-        self.epochs = 10
-        self.max_doc_len = 128
+
         self.bow = set()
         self.bow_path = "data/stackex/bow.data"
-
         self.vocab_path = "data/stackex/vocab.data"
+        self.summ_intv = 1000
+        self.epochs = 1000000
+        self.sample_path = "data/stackex/samples.json"
         #self.onehot_encoder = OneHotEncoder()
         #self.onehot_encoder_path = "data/stackex/onehot_encoder.data"
         self.prepare()
@@ -68,18 +124,53 @@ class StackEx(object):
             text = re.sub("<.*?>", " ", text)
             text = ' '.join(text.split()).strip()
             yield text
-
+    
     def build_model(self):
-        pass
+        self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim])
+        self.X = tf.placeholder(tf.float32, shape=[None, self.x_dim])
 
-    def foreach_epoch(self):
+        self.Q = Q(self.z_dim, self.h_dim, self.x_dim)
+        self.P = P(self.z_dim, self.h_dim, self.x_dim)
+
+        self.z_samples = self.Q(self.X)
+        _, self.logits = self.P(self.z_samples)
+        self.samples, _ = self.P(self.z)
+
+        self.kl_loss = 0.5 * tf.reduce_sum(tf.exp(self.logits))
+        self.recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(\
+                logits=self.logits, labels=self.X), 1)
+        self.vae_loss = tf.reduce_mean(self.kl_loss + self.recon_loss)
+
+        self.global_step = tf.Variable(self.init_step)
+        self.vae_train_op = tf.train.AdamOptimizer().minimize(self.vae_loss, global_step=self.global_step)
+
+    def foreach_epoch(self, sess):
         for e in range(self.epochs):
-            self.foreach_step()
+            self.foreach_step(sess)
 
-    def foreach_step(self):
+    def foreach_step(self, sess):
         for X in self.gen_data():
-            X = self.vocab_processor.transform(X)
-            #TODO
+            X = list(self.vocab_processor.transform(X))
+            if not X:
+                continue
+            _, loss, step = sess.run([self.vae_train_op, self.vae_loss, self.global_step], \
+                feed_dict={self.X: X})
+
+            if step % self.summ_intv == 0:
+                print('[step/{}] loss:{:.4}'.format(step, loss))
+                samples = sess.run(self.samples, feed_dict={self.z: np.random.randn(50, self.z_dim)})
+                docs = list(self.vocab_processor.reverse(samples.astype(np.int)))
+                self.to_json({'sample': docs}, self.sample_path)
+
+    def to_json(self, obj, output_path):
+        with open(output_path, 'a') as fd:
+            fd.write(ujson.dumps(obj)+'\n')
+
+    def train(self):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for _ in range(self.epochs):
+                self.foreach_epoch(sess)
 
     def build_bow(self):
         for X in self.gen_data():
@@ -103,8 +194,8 @@ class StackEx(object):
 #        self.bow = list(self.bow)
         pass
 
-
 if __name__ == '__main__':
     stex = StackEx()
     #list(stex.gen_data())
-    stex.preprocess()
+    #stex.preprocess()
+    stex.train()
