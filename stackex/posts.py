6386c43aea2b8a5de25a48655eb800915a0d6338
Zex Li <top_zlynch@yahoo.com>
2018-03-09 18:18:24 +0800

    finetune: kl loss

--- stackex/posts.py
+++ stackex/posts.py
@@ -3,6 +3,7 @@
 # Author: Zex Li <top_zlynch@yahoo.com>
 import os
 import re
+import string
 from lxml import etree
 import pickle
 import numpy as np
@@ -34,18 +35,18 @@ class P:
 
     def __call__(self, z):
         with tf.name_scope('P'):
-            self.w1 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.z_dim, self.h_dim]))
-            self.b1 = tf.Variable(tf.zeros([self.h_dim]))
-            self.w2 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.x_dim]))
-            self.b2 = tf.Variable(tf.zeros([self.x_dim]))
+            self.w_x = tf.Variable(tf.contrib.layers.xavier_initializer()([self.z_dim, self.h_dim]))
+            self.b_x = tf.Variable(tf.zeros([self.h_dim]))
+            self.w_log = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.x_dim]))
+            self.b_log = tf.Variable(tf.zeros([self.x_dim]))
 
-            h = tf.nn.relu(tf.nn.xw_plus_b(z, self.w1, self.b1, name='l_1'))
-            logits = tf.nn.xw_plus_b(h, self.w2, self.b2, name='l_2')
+            h = tf.nn.relu(tf.nn.xw_plus_b(z, self.w_x, self.b_x))
+            logits = tf.nn.xw_plus_b(h, self.w_log, self.b_log)
             prob = tf.nn.sigmoid(logits)
-        return prob, logits
+            return prob, logits
 
     def var_list(self):
-        return [self.w1, self.b1, self.w2, self.b2]
+        return [self.w_x, self.w_log, self.b_x, self.b_log]
 
 
 class Q:
@@ -58,31 +59,35 @@ class Q:
 
     def __call__(self, X):
         with tf.name_scope('Q'):
-            self.w1 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.x_dim, self.h_dim]))
-            self.b1 = tf.Variable(tf.zeros([self.h_dim]))
-            self.w2 = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.z_dim]))
-            self.b2 = tf.Variable(tf.zeros([self.z_dim]))
+            #x = tf.nn.batch_normalization(X, 100.258, 100.323, 0.24, 1., 1e-10)
+            self.w_x = tf.Variable(tf.contrib.layers.xavier_initializer()([self.x_dim, self.h_dim]))
+            self.b_x = tf.Variable(tf.zeros([self.h_dim]))
+            self.w_mu = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.z_dim]))
+            self.b_mu = tf.Variable(tf.zeros([self.z_dim]))
+            self.w_sigma = tf.Variable(tf.contrib.layers.xavier_initializer()([self.h_dim, self.z_dim]))
+            self.b_sigma = tf.Variable(tf.zeros([self.z_dim]))
 
-            h = tf.nn.relu(tf.nn.xw_plus_b(X, self.w1, self.b1, name='l_1'))
-            z = tf.nn.relu(tf.nn.xw_plus_b(h, self.w2, self.b2, name='l_2'))
-        return h, z
+            h = tf.nn.sigmoid(tf.nn.xw_plus_b(X, self.w_x, self.b_x))
+            mu = tf.nn.xw_plus_b(h, self.w_mu, self.b_mu)
+            z = tf.nn.xw_plus_b(h, self.w_sigma, self.b_sigma)
+            return mu, z
 
     def var_list(self):
-        return [self.w1, self.b1, self.w2, self.b2]
+        return [self.w_x, self.b_x, self.w_mu, self.b_mu, self.w_sigma, self.b_sigma]
 
 
 class StackEx(object):
 
     def __init__(self):
-        self.max_doc_len = 128 #256
+        self.max_doc_len = 256 #128 #256
 
         self.z_dim = 128
         self.x_dim = self.max_doc_len
         self.h_dim = 128
 
-        self.init_step = 0
+        self.init_step = 1
         self.data_path = "data/ai.stackexchange.com/Posts.xml"
-        self.batch_size = 32
+        self.batch_size = 256
 
         self.bow = set()
         self.bow_path = "data/stackex/bow.data"
@@ -90,7 +95,7 @@ class StackEx(object):
         self.sample_path = "data/stackex/samples.json"
         self.summ_intv = 1000
         self.epochs = 1000000
-        self.lr = 1e-4
+        self.lr = 1e-3
         self.prepare()
 
     def build_vocab_processor(self):
@@ -103,7 +108,9 @@ class StackEx(object):
         else:
             X = list(map(lambda x: x, self.gen_data()))
             self.vocab_processor.fit(X)
+            list(map(lambda c: self.vocab_processor.vocabulary_._mapping.get(c), string.punctuation))
             self.vocab_processor.save(self.vocab_path)
+            print("[info] vocab:{}".format(len(self.vocab_processor.vocabulary_)))
 
     def prepare(self):
         self.build_vocab_processor()
@@ -118,22 +125,27 @@ class StackEx(object):
         with open(self.data_path) as fd:
             tree = etree.XML(fd.read())
         items = tree.xpath('row')
-        batch = []
 
         for i, item in enumerate(items):
             text = item.attrib.get('Body')
             text = re.sub("<.*?>", " ", text)
             text = ' '.join(text.split()).strip()
+            yield text
 
+    def gen_batch(self):
+        batch = []
+
+        for sample in self.gen_data():
             if len(batch) == self.batch_size:
-                yield batch
+                yield np.array(batch)
                 batch.clear()
             else:
-                batch.append(text)
+                batch.append(' '.join(sample.split()))
     
-    def samples(self, mu, var):
-        eps = tf.random_normal(shape=tf.shape(mu))
-        return mu + eps * tf.exp(var)
+    def gaussian_samples(self, mu, var):
+        with tf.name_scope('gaussian_samples'):
+            eps = tf.random_normal(shape=tf.shape(mu))
+            return mu + tf.exp(var/2) * eps
 
     def build_model(self):
         self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim])
@@ -142,43 +154,45 @@ class StackEx(object):
         self.Q = Q(self.z_dim, self.h_dim, self.x_dim)
         self.P = P(self.z_dim, self.h_dim, self.x_dim)
 
+        #self.X_norm = tf.nn.batch_normalization(self.X, 256, 223, 0., 1., 1e-8)
         self.mu, self.var = self.Q(self.X)
-        self.z_samples = self.samples(self.mu, self.var)
+        self.z_samples = self.gaussian_samples(self.mu, self.var)
         _, self.logits = self.P(self.z_samples)
         self.samples, _ = self.P(self.z)
         
-        self.kl_loss = 0.5 * tf.reduce_sum(tf.exp(2*self.var) - 1.+ self.mu**2, 1)
+        self.kl_loss = 0.5 * tf.reduce_sum(tf.exp(2 * self.var) - 1.+ self.mu**2, 1)
         self.recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(\
-                logits=self.samples, labels=self.X), 1)
+                logits=self.logits, labels=self.X), 1)
         self.vae_loss = tf.reduce_mean(self.kl_loss + self.recon_loss)
-
+        
         self.global_step = tf.Variable(self.init_step)
         self.vae_train_op = tf.train.AdamOptimizer(self.lr).minimize(\
                 self.vae_loss, \
-                global_step=self.global_step, \
-                var_list=self.Q.var_list()+self.P.var_list())
+                global_step=self.global_step,\
+                var_list=self.P.var_list()+self.Q.var_list())
 
     def foreach_epoch(self, sess):
         for e in range(self.epochs):
             self.foreach_step(sess)
 
     def foreach_step(self, sess):
-        for X in self.gen_data():
+        for X in self.gen_batch():
             X = list(self.vocab_processor.transform(X))
             
             if not X:
                 continue
 
+            X = np.array(X).astype(np.float32)
             z_data = np.random.randn(self.batch_size, self.z_dim)
-            _, loss, z_samples, step, mu, var = sess.run(
-                    [self.vae_train_op, self.vae_loss, self.z_samples, self.global_step, self.Q.w1, self.Q.w2], \
+
+            mu, var, _, loss, z_samples, step = sess.run(
+                    [self.z_samples, self.P.w_x, self.vae_train_op, self.vae_loss, self.z_samples, self.global_step],\
                 feed_dict={
                     self.X: X,
                     self.z: z_data,
                     })
-            print(mu, var)
-            print('[step/{}] {} loss:{:.4}'.format(step, datetime.now(), loss))
             if step % self.summ_intv == 0:
+                print('[step/{}] {} loss:{:.4}'.format(step, datetime.now(), loss))
                 samples = sess.run(self.samples, feed_dict={self.z: z_data})
                 docs = list(self.vocab_processor.reverse(samples.astype(np.int)))
                 self.to_json({'sample': docs}, self.sample_path)
